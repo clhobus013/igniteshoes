ea5dbc365e1b7ccc1f8bab8d3ad9f3c95dcdb95e
Claudia Hobus <clauu013@gmail.com>
2025-03-29 17:33:21 -0300

    configuracao oneSignal

--- App.tsx
+++ App.tsx
@@ -9,6 +9,10 @@ import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 
+import { OneSignal } from 'react-native-onesignal';
+
+OneSignal.initialize("ebc3dab6-c24b-49df-9de3-6618904569bd");
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
